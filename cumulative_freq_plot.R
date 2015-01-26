6d74dec10cbed3310cdf333f35ac79b0cdd5132f
Abby Mullen <abby@lincolnmullen.com>
2015-01-25 20:02:32 -0500

    Recalculated regex to put the right dates on the data

--- cumulative_freq_plot.R
+++ cumulative_freq_plot.R
@@ -5,20 +5,24 @@ dates <- read.csv("Papers_start_end.csv", sep=',')
 
 starts <- dates$CA_start
 
-breaks <- seq(1836, 1860, by=1)
+starts_freq <- table(starts)
+
+breaks <- seq(1836, 1861, by=1)
 starts.cut <- cut(starts, breaks, right=FALSE)
 starts.freq <- table(starts.cut)
 starts.cumfreq <- cumsum(starts.freq)
 cumulative <- cbind(starts.cumfreq)
 cumulative_freq <- as.data.frame(cumulative)
+write.csv(cumulative_freq, file = "Cumulative_Frequency.csv")
 
 cumulative_freq <- read.csv('Cumulative_Frequency.csv', sep=',')
 
 cumulative_freq$year <- cumulative_freq$year_range %>%
   as.character %>%
-  gsub(".*([0-9]{4}).*","\\1",.)
+  sub(".([0-9]{4}).*",'\\1',.)
 
-freq <- data.frame(year = 1837:1860, cumulative = as.vector(cumulative_freq$starts.cumfreq))
+
+freq <- data.frame(year = 1836:1860, cumulative = as.vector(cumulative_freq$starts.cumfreq))
 
 p <- ggplot(freq, aes(x=year, y=cumulative))
 p + geom_point() +
@@ -27,7 +31,8 @@ p + geom_point() +
   ggtitle("Cumulative Number of Papers in CA by Date")
 
 r <- ggplot(freq, aes(x=year)) 
-r + geom_area(aes(y=cumulative)) +
+r + geom_area(aes(y=cumulative), fill='gray', colour='black') +
   xlab("Year") +
   ylab("Cumulative number of papers") +
   ggtitle("Cumulative Number of Papers in CA by Date")
+
