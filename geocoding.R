8440b7c0ff84b1dbdaabf54f87a5427d10310ca5
Abby Mullen <abby@lincolnmullen.com>
2015-03-27 23:24:33 -0400

    Fell behind on commits

--- geocoding.R
+++ geocoding.R
@@ -2,16 +2,22 @@ library(dplyr)
 library(ggmap)
 library(ggplot2)
 
-text = read.csv("ThereIsNoDeath.tsv"
-                   , sep = '\t', quote = "", header = TRUE, stringsAsFactors = FALSE)
+text = read.csv("Attribution Study/BeautifulSnow.tsv"
+                   , sep = '\t', quote = "", header = TRUE, stringsAsFactors = FALSE)  %>%  
+  #mutate(paper_title = gsub('(.*)\\.*\\(.*','\\1',paper)) %>%
+  #mutate(location = gsub('.*\\((.*)\\).*','\\1',paper)) %>%
+  mutate(date = gsub('(\\d+)/(\\d+)/(\\d{4})','\\3-\\1-\\2',date)) %>%
+  mutate(year = gsub('(\\d{4})-(\\d+)-(\\d+)','\\1',date)) %>%
+  select(publication,location,year,date,author) %>%
+  mutate(date = as.Date(date)) %>%
+  mutate(end_date = date + 730)
 
-geocode = geocode(c(text$location))
+geocode = geocode(c(as.character(text$location))) 
 
 text$lat <- geocode$lat
 text$lon <- geocode$lon
-text$date_formatted <- gsub('(\\d+)/(\\d+)/(\\d{4})','\\3-\\1-\\2',text$date)
 
 text_geo <- text %>%
-  select(paper,location,paratext,lat,lon,date_formatted)
+  select(year,date,end_date,publication,author,location,lat,lon)
 
-write.table(text_geo, file = 'dickinson_children_250,121.txt', sep = '\t')
+write.table(text_geo, file = 'BeautifulSnow_geocode.txt', sep = '\t')
