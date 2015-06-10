7cdfe30cc9a5e53a03a32d67bf1d6f66068bef1f
Abby Mullen <abby@lincolnmullen.com>
2015-06-10 10:46:47 -0400

    Added some explanatory comments

--- geocoding.R
+++ geocoding.R
@@ -2,22 +2,22 @@ library(dplyr)
 library(ggmap)
 library(ggplot2)
 
-text = read.csv("Attribution Study/BeautifulSnow.tsv"
-                   , sep = '\t', quote = "", header = TRUE, stringsAsFactors = FALSE)  %>%  
-  #mutate(paper_title = gsub('(.*)\\.*\\(.*','\\1',paper)) %>%
-  #mutate(location = gsub('.*\\((.*)\\).*','\\1',paper)) %>%
-  mutate(date = gsub('(\\d+)/(\\d+)/(\\d{4})','\\3-\\1-\\2',date)) %>%
-  mutate(year = gsub('(\\d{4})-(\\d+)-(\\d+)','\\1',date)) %>%
-  select(publication,location,year,date,author) %>%
-  mutate(date = as.Date(date)) %>%
-  mutate(end_date = date + 730)
+text = read.csv("Attribution_Study/BeautifulSnow/BeautifulSnow_count.tsv"
+                   , header = TRUE, stringsAsFactors = FALSE,sep = '\t') %>%
+  filter(location != "unknown") %>%
+  #mutate(paper_title = gsub('(.*)\\.*\\(.*','\\1',paper)) %>% #This separates the paper's actual name out
+  #mutate(location = gsub('.*\\((.*)\\).*','\\1',paper)) %>% #This separates out the location from the name
+  mutate(date = gsub('(\\d+)/(\\d+)/(\\d{4})','\\3-\\1-\\2',date)) %>% #puts date in ISO format
+  mutate(year = gsub('(\\d{4})-(\\d+)-(\\d+)','\\1',date)) %>% #extracts year
+  select(publication,location,year,date,author) %>% #selects columns that you want to see
+  mutate(date = as.Date(date)) %>% #turns date (text) into a date type so you can use it for calculations
+  mutate(end_date = date + 730) #this is to add two years for the time lapse--comment out if not needed
 
 geocode = geocode(c(as.character(text$location))) 
 
-text$lat <- geocode$lat
+text$lat <- geocode$lat #this is a simple way to combine the dataframes like you want them
 text$lon <- geocode$lon
 
-text_geo <- text %>%
-  select(year,date,end_date,publication,author,location,lat,lon)
 
-write.table(text_geo, file = 'BeautifulSnow_geocode.txt', sep = '\t')
+write.table(text, file = 'Attribution_Study//BeautifulSnow//BeautifulSnow_geocode.txt'
+            , sep = '\t',row.names = FALSE)
