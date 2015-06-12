c2cea3032262338998f23b8563aae7ee81e2bc34
Abby Mullen <abby@lincolnmullen.com>
2015-06-12 08:06:06 -0400

    took out some junk

--- geocode_unique.R
+++ geocode_unique.R
@@ -6,11 +6,8 @@ text = read.csv("FullC19-working notes (1).csv"
                 , header = FALSE, stringsAsFactors = FALSE,sep = ',') %>% 
   mutate(paper_title = gsub('(.*)\\.*\\(.*','\\1',V4)) %>% #This separates the paper's actual name out
   mutate(location = gsub('.*\\((.*)\\).*','\\1',V4)) %>% #This separates out the location from the name
-  # mutate(date = gsub('(\\d+)/(\\d+)/(\\d{4})','\\3-\\1-\\2',V3)) %>% #puts date in ISO format
   mutate(year = gsub('(\\d{4})-(\\d+)-(\\d+)','\\1',V3)) %>% #extracts year
-  # select(publication,location,year,date,author) %>% #selects columns that you want to see
   mutate(date = as.Date(V3)) %>%  #turns date (text) into a date type so you can use it for calculations
-  #  mutate(end_date = date + 730) %>% #this is to add two years for the time lapse--comment out if not needed
   mutate(text = V5) %>% 
   select(date,year,paper_title,text,location)
 
